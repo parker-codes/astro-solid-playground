5fc9e3c5de220a6377dd3463f8419650cf95b8c5
Parker McMullin <parker.mcmullin01@gmail.com>
2023-12-17 00:20:10 -0700

    Clean up. Use If component.

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,4 +1,4 @@
-import { createSignal, Show } from 'solid-js';
+import { createSignal, Show, type JSX } from 'solid-js';
 // @ts-expect-error No typings
 import Sortable from 'solid-sortablejs';
 import { createLocalStorageSignal } from '../hooks';
@@ -73,21 +73,7 @@ export default function Todos() {
 								class='px-2 rounded-full bg-transparent hover:bg-red-900 text-red-400 aspect-square transition-colors duration-150'
 								aria-label='Delete item'
 							>
-								{/* biome-ignore lint/a11y/noSvgWithoutTitle: explain on button */}
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									fill='none'
-									viewBox='0 0 24 24'
-									stroke-width='1.5'
-									stroke='currentColor'
-									class='w-5 h-5'
-								>
-									<path
-										stroke-linecap='round'
-										stroke-linejoin='round'
-										d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'
-									/>
-								</svg>
+								<TrashIcon />
 							</button>
 
 							<button
@@ -102,44 +88,11 @@ export default function Todos() {
 									todo.completed ? 'Mark as not completed' : 'Mark as completed'
 								}
 							>
-								<Show
-									when={todo.completed}
-									fallback={
-										<>
-											{/* biome-ignore lint/a11y/noSvgWithoutTitle: explain on button */}
-											<svg
-												xmlns='http://www.w3.org/2000/svg'
-												fill='none'
-												viewBox='0 0 24 24'
-												stroke-width='1.5'
-												stroke='currentColor'
-												class='w-5 h-5'
-											>
-												<path
-													stroke-linecap='round'
-													stroke-linejoin='round'
-													d='M4.5 12.75l6 6 9-13.5'
-												/>
-											</svg>
-										</>
-									}
-								>
-									{/* biome-ignore lint/a11y/noSvgWithoutTitle: explain on button */}
-									<svg
-										xmlns='http://www.w3.org/2000/svg'
-										fill='none'
-										viewBox='0 0 24 24'
-										stroke-width='1.5'
-										stroke='currentColor'
-										class='w-5 h-5'
-									>
-										<path
-											stroke-linecap='round'
-											stroke-linejoin='round'
-											d='M19.5 12h-15'
-										/>
-									</svg>
-								</Show>
+								<If
+									cond={todo.completed}
+									then={<CheckIcon />}
+									else={<DashIcon />}
+								/>
 							</button>
 						</div>
 					</div>
@@ -147,4 +100,70 @@ export default function Todos() {
 			</Sortable>
 		</>
 	);
+}
+
+function TrashIcon() {
+	return (
+		<>
+			{/* biome-ignore lint/a11y/noSvgWithoutTitle: explain on button */}
+			<svg
+				xmlns='http://www.w3.org/2000/svg'
+				fill='none'
+				viewBox='0 0 24 24'
+				stroke-width='1.5'
+				stroke='currentColor'
+				class='w-5 h-5'
+			>
+				<path
+					stroke-linecap='round'
+					stroke-linejoin='round'
+					d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'
+				/>
+			</svg>
+		</>
+	);
+}
+
+function DashIcon() {
+	return (
+		<>
+			{/* biome-ignore lint/a11y/noSvgWithoutTitle: explain on button */}
+			<svg
+				xmlns='http://www.w3.org/2000/svg'
+				fill='none'
+				viewBox='0 0 24 24'
+				stroke-width='1.5'
+				stroke='currentColor'
+				class='w-5 h-5'
+			>
+				<path
+					stroke-linecap='round'
+					stroke-linejoin='round'
+					d='M4.5 12.75l6 6 9-13.5'
+				/>
+			</svg>
+		</>
+	);
+}
+
+function CheckIcon() {
+	return (
+		<>
+			{/* biome-ignore lint/a11y/noSvgWithoutTitle: explain on button */}
+			<svg
+				xmlns='http://www.w3.org/2000/svg'
+				fill='none'
+				viewBox='0 0 24 24'
+				stroke-width='1.5'
+				stroke='currentColor'
+				class='w-5 h-5'
+			>
+				<path stroke-linecap='round' stroke-linejoin='round' d='M19.5 12h-15' />
+			</svg>
+		</>
+	);
+}
+
+function If(props: { cond: boolean; then: JSX.Element, else?: JSX.Element }) {
+	return <>{props.cond ? props.then : props.else ?? null}</>;
 }
